39dc66e4d4697a0a3b5d252683a7bad26586b580
spainisnotequal <spainisnotequal@gmail.com>
2020-11-22 19:56:14 +0100

    Wrap both static methods and instance methods into Lisp functions

--- extract-text.lisp
+++ extract-text.lisp
@@ -29,17 +29,20 @@
 (defconstant +pd-document+ (jclass "org.apache.pdfbox.pdmodel.PDDocument"))
 (defconstant +pdf-text-stripper+ (jclass "org.apache.pdfbox.text.PDFTextStripper"))
 
-;; Import the methods we will use
-(defconstant +load+ (jmethod +pd-document+ "load" +file+))
-(defconstant +get-text+ (jmethod +pdf-text-stripper+ "getText" +pd-document+))
+;;; Wrap both static methods and instance methods into Lisp functions
+(defun load-document (filepath)
+  (let ((file (jnew +file+ filepath)))
+    (jstatic (jmethod +pd-document+ "load" +file+) +pd-document+ file)))
+
+(defun get-text (document)
+  (let ((text (jnew +pdf-text-stripper+)))
+    (jcall (jmethod +pdf-text-stripper+ "getText" +pd-document+) text document)))
 
 ;; Define the function to extract the content of a PDF file
 (defun extract-text-from-pdf (filepath)
   "Given the path of a PDF file, extract its content as text."
-  (let* ((file (jnew +file+ filepath))
-         (document (jstatic +load+ +pd-document+ file))
-         (text (jcall +get-text+ (jnew +pdf-text-stripper+) document)))
-    text))
+  (let ((document (load-document filepath)))
+    (get-text document)))
 
 ;; Test the function extracting the content of the file "test.pdf"
 (extract-text-from-pdf "./test.pdf")
